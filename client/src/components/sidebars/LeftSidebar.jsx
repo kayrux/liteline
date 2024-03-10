0da1547686450cadce6618ed109d19f8609bd603
adrian.llonor <adrianllonor@gmail.com>
2024-03-09 21:39:14 -0700

    Dynamically maps rooms and members to sidebar
    Working on queries to DB

--- client/src/components/sidebars/LeftSidebar.jsx
+++ client/src/components/sidebars/LeftSidebar.jsx
@@ -3,15 +3,32 @@ import CreateRoom from "../popups/CreateRoom";
 import UserSettings from "../popups/UserSettings";
 import Room from "../member/Room";
 import React from 'react';
+import { useState } from 'react';
 
-// TODO: Test room data array until I connect back end
-var roomsData = [{name: 'CPSC 559 Discussion Group', link: 'roomCode'}, 
-{name: 'Legend of Lonk', link: 'roomCode'},
-{name: 'ASDF MAN', link: 'roomCode'}];
-
-// TODO: Load in room data from backend
 
 const LeftSidebar = (props) => {
+
+  const [roomArr, updateArr] = useState([{name: 'CPSC 559 Discussion Group', link: 'roomCode'}, {name: 'Legend of Lonk', link: 'roomCode'}, {name: 'zxcv', link: 'roomCode'}])
+  const { rooms } = []
+
+  // TODO: Make useGetUserRooms() return list of rooms by api
+
+  // A function that updates the list of rooms from the database
+  const fetchRoomList = () =>{
+    try {
+      // const [userPacket, { isLoading }] = <useGetUserRooms()> // Query and get list of rooms for our user with yet to be made function useGetUserRooms()
+      // const res = userPacket({ rooms }).unwrap(); // Unwrap the room names and save it into rooms
+      //updateArr(rooms)
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // Once a user leaves a room/joins another room, they load that page so we update the list of rooms
+  window.addEventListener("load", fetchRoomList())
+
+
   return (
     <div className="flex flex-col justify-between w-1/5 min-w-fit h-full">
       <h1 className="text-lg font-bold mt-4 text-center">Available Rooms</h1>
@@ -23,7 +40,7 @@ const LeftSidebar = (props) => {
         >
 
         {/* Dynamically add in the rooms loaded into "roomsData" above */}
-        {roomsData.map(roomComponent => (
+        {roomArr.map(roomComponent => (
           <Room name={roomComponent.name} link={roomComponent.link} />
         ))}
 
--- client/src/components/sidebars/RightSidebar.jsx
+++ client/src/components/sidebars/RightSidebar.jsx
@@ -2,18 +2,36 @@ import RoomSettings from "../popups/RoomSettings";
 import ShareRoom from "../popups/ShareRoom";
 import Member from "../member/Member";
 import React from 'react';
+import { useState } from 'react';
 
 const RightSidebar = (props) => {
 
-  // TODO: Placeholder array until I connect to the backend
-  var membersData = [{name: 'You', isOwner: 'true', isAFK: 'false'}, 
-    {name: 'Jennifer Lawrence', isOwner: 'false', isAFK: 'false'},
-    {isOwner: 'false', isAFK: 'true'}];
+  // Make the array of rooms mutatable 
+  const [memberArr, updateArr] = useState([{uid: '', name: 'You', isOwner: 'true', isAFK: 'false'}, 
+    {uid: '', name: 'Jennifer Lawrence', isAFK: 'false'},
+    {uid: '', isAFK: 'true'}])
+  const { members } = []
 
-  // TODO: Load in member data from backend
+  // TODO: Make useGetMembers() return list of members by api
 
-  // TODO: When membersData changes, refresh "memberContainer"
+  // A function that updates the members in the member list
+  // Intended to be called on page load and when number of websocket connections changes
+  const updateMemberList = () =>{
+    try {
+      // const [memberList, { isLoading }] = <useGetMembers()> // Query and get list of members in the room
+      // const res = memberList({ members }).unwrap() // Unwrap the room names and save it into rooms
+      // updateArr(members) // Update here
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
+  // Load initial list of members when page is loading in
+  window.addEventListener("load", updateMemberList())
+  
+  // TODO: When # connections changes in websocket, update active status and check if member list has changed
+
+  // TODO: onMouseMove, make active. If moving from inactive->active, propagate on web sockets. After set amount of time, set status to inactive and propagate on web sockets
 
   return (
     <div className="flex flex-col justify-between h-full w-1/5 min-w-fit">
@@ -25,7 +43,7 @@ const RightSidebar = (props) => {
           className="overflow-y-auto flex flex-col items-center items-stretch space-y-3 h-full"
         >
           {/* Dynamically load in members from membersData array */}
-          {membersData.map(membersComponent => (
+          {memberArr.map(membersComponent => (
             <Member name={membersComponent.name} 
                     isOwner={membersComponent.isOwner} 
                     isAFK={membersComponent.isAFK} />
