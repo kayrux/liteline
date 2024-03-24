8bcfa6eae4d085bb2e4e41fccb2065a0dda87dc6
kayrux <skusjanto@outlook.com>
2024-03-24 13:11:44 -0600

    duplicate servers. changed localhost ports

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-const serverUrl = process.env.REACT_APP_SERVER_URL || "http://localhost:8000";
+const serverUrl = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
 const socket = io(serverUrl, {
   autoConnect: false,
   withCredentials: true,
