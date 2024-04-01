93af11395be622c6fe1a6e2cf7fadab89c30960e
kayrux <skusjanto@outlook.com>
2024-03-31 22:05:14 -0600

    Update client to use new proxy url

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -3,7 +3,8 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl:
-      "http://localhost:5000" || "https://liteline-api.azurewebsites.net", // server url
+      "https://liteline-proxy.onrender.com" ||
+      "https://liteline-api.azurewebsites.net", // server url
     credentials: "include",
   }),
   tagTypes: ["User", "Room", "Message"],
